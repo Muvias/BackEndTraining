1f2331c83e45e2e8e1d622178e1c363e3c358daf
Vinicius Perazza <vi_guepardo@hotmail.com>
2021-09-01 01:08:11 -0300

    MaratonaDev

--- src/routes.js
+++ src/routes.js
@@ -56,14 +56,14 @@ const Job = {
             name: "Pizzaria Guloso",
             "daily-hours": 2,
             "total-hours": 60,
-            created_at: Date.now()
+            created_at: Date.now(),
         },
         {
             id: 2,
             name: "OneTwo Project",
             "daily-hours": 3,
             "total-hours": 47,
-            created_at: Date.now()
+            created_at: Date.now(),
         }
     ],
 
@@ -78,19 +78,19 @@ const Job = {
                     ...job,
                     remaining,
                     status,
-                    budget: Profile.data[("value-hour")] * job[("total-hours")]
+                    budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
                 }
             })
             
             return res.render(views + "index", { jobs: updatedJobs })
         },
 
-        create(req, res){
+        create(req, res) {
             return res.render(views + "job")
         },
 
         save(req, res) {
-            const lastId = Job.data[Job.data.length - 1]?.id || 1;
+            const lastId = Job.data[Job.data.length - 1]?.id || 0;
 
             Job.data.push({
                 id: lastId + 1,
@@ -99,6 +99,56 @@ const Job = {
                 "total-hours": req.body["total-hours"],
                 created_at: Date.now() // atibuindo data do momento que o formulário for enviado
             })
+            return res.redirect('/')
+        },
+
+        show(req, res) {
+
+            const jobId = req.params.id
+
+            const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+            if (!job) {
+                return res.send('Job not found!')
+            }
+
+            job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
+
+            return res.render(views + "job-edit", { job })
+        },
+
+        update(req, res) {
+            const jobId = req.params.id
+
+            const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+            if (!job) {
+                return res.send('Job not found!')
+            }
+
+            const updatedJob = {
+                ...job,
+                name: req.body.name,
+                "total-hours": req.body["total-hours"],
+                "daily-hours": req.body["daily-hours"]
+            }
+
+            Job.data = Job.data.map(job => {
+                if(Number(job.id) === Number(jobId)){
+                    job = updatedJob
+                }
+
+                return job
+            })
+
+            res.redirect('/job/' + jobId)
+        },
+
+        delete(req, res) {
+            const jobId = req.params.id
+
+            Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId))
+
             return res.redirect('/')
         }
     },
@@ -119,7 +169,8 @@ const Job = {
     
             //restam x dias
             return dayDiff
-        }
+        },
+        calculateBudget: (job, valueHour) => valueHour * job[("total-hours")]
     }
 }
 
@@ -127,7 +178,9 @@ const Job = {
 routes.get('/', Job.controllers.index)
 routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save) // Enviando os dados coletados pelo formulário
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/job/:id', Job.controllers.show)
+routes.post('/job/:id', Job.controllers.update)
+routes.post('/job/delete/:id', Job.controllers.delete)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
