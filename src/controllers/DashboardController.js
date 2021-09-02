89e30912e073b1303c5a2dd72b7a794cd50d9db3
Vinicius Perazza <vi_guepardo@hotmail.com>
2021-09-01 22:19:07 -0300

    maratonaDev

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -7,11 +7,27 @@ module.exports = {
         const jobs = Job.get();
         const profile = Profile.get();
     
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: jobs.length
+        }
+
+        // total de horas por dia de cada projeto em progress
+        let jobTotalHours = 0
+
         const updatedJobs = jobs.map((job) => {
             //ajustes no job
             const remaining = JobUtils.remainingDays(job)
             const status = remaining <= 0 ? 'done' : 'progress'
+
+            // status = done or progress
+            // statusCount[done or progress] += 1
+            // atribuindo a quantidade ao statusCount
+            statusCount[status] += 1;
             
+            jobTotalHours = status == 'progress' ? jobTotalHours + Number(job['daily-hours']) : jobTotalHours
+
             return {
                 ...job,
                 remaining,
@@ -20,6 +36,9 @@ module.exports = {
             }
         })
         
-        return res.render("index", { jobs: updatedJobs })
+        // quantidade de hrs que quero trabalhar/dia - quantidade de h/dias de cada job em progress
+        const freeHours = profile["hours-per-day"] - jobTotalHours;
+
+        return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours })
     }
 }
